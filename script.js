e0fd64ebadc732a87f6129b7734a26bff2f1a322
mahaMohamed <maha.fagal@gmail.com>
2020-11-15 12:53:34 +0200

    Optimized game

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let turns = 9;
     /*Attaching the cells to the grid*/
     for (let i = 0; i < 9; i++) {
         div.setAttribute("id", i);
+        // div.setAttribute("move" , i);
         gameGridContainer.appendChild(div.cloneNode());
 
     }
@@ -75,12 +76,13 @@ const game = (function (gameGrid) {
 
     const _isThereAWinner = function (x, y, z) {
 
-        if (gameGrid[x].innerHTML === gameGrid[y].innerHTML &&
-            gameGrid[y].innerHTML === gameGrid[z].innerHTML &&
-            gameGrid[x].innerHTML != "") {
+        if (gameGrid[x].moveMark === gameGrid[y].moveMark &&
+            gameGrid[y].moveMark === gameGrid[z].moveMark &&
+           ( gameGrid[x].moveMark == "x" || gameGrid[x].moveMark == "o")) {
+            //    console.log(x,y,z);
             return {
                 win: true,
-                winnerMark: gameGrid[x].innerHTML,
+                winnerMark: gameGrid[x].moveMark,
                 winningSequence: [x, y, z]
             }
         }
@@ -136,20 +138,24 @@ const game = (function (gameGrid) {
         if (turns <= 0)
             return;
 
-        // let randomNumber = _getRandomInt();
+        let randomNumber = _getRandomInt();
 
-        // while (true) {
-        //     randomNumber = _getRandomInt();
-        //     if (markedCell[randomNumber] == false) {
-        //         break;
-        //     }
-        // }
+        while (true) {
+            randomNumber = _getRandomInt();
+            if (markedCell[randomNumber] == false) {
+                break;
+            }
+        }
 
         // gameGrid[randomNumber].innerHTML = computerPlayer.getPlayerMark();
+        // markedCell[randomNumber] = true;
+        // gameGrid[randomNumber].moveMark = computerPlayer.getPlayerMark();
 
-        let computerBestMove = minimax(gameGrid, computerPlayer).id;
+        let computerBestMove = minimax(gameGrid, computerPlayer, 0).id;
+
         gameGrid[computerBestMove].innerHTML = computerPlayer.getPlayerMark();
         markedCell[computerBestMove] = true;
+        gameGrid[computerBestMove].moveMark = computerPlayer.getPlayerMark();
 
     }
 
@@ -176,10 +182,9 @@ const game = (function (gameGrid) {
 
     }
 
-    const minimax = function (gameGrid, player) {
+    const minimax = function (gameGrid, player, depth) {
 
         //Base case
-        let moves = [];
         // debugger;
 
         // console.log(JSON.stringify(moves));
@@ -187,15 +192,16 @@ const game = (function (gameGrid) {
         if (winner !== null) {
 
             if (winner.winnerMark === humanPlayer.getPlayerMark()) {
-                console.log("here");
+
+
                 return {
-                    evaluation: -10
+                    evaluation: depth - 10
                 }
             }
 
             else if (winner.winnerMark === computerPlayer.getPlayerMark()) {
                 return {
-                    evaluation: 10
+                    evaluation: 10 - depth
                 }
             }
         }
@@ -209,6 +215,7 @@ const game = (function (gameGrid) {
 
 
         let emptySpaces = _getEmptySpaces();
+        let moves = [];
 
 
         //Looping over empty spaces in the grid to play each one of them and evaluate their value 
@@ -220,30 +227,32 @@ const game = (function (gameGrid) {
             let id = emptySpaces[i];
             move.id = id;
 
-            let backup = gameGrid[id].innerHTML;
+            let backup = undefined;
 
             //Playing the empty space
-            gameGrid[id].innerHTML = player.getPlayerMark();
+            gameGrid[id].moveMark = player.getPlayerMark();
             markedCell[id] = true;
 
 
 
             //Recursive Call
             if (player.getPlayerMark() === computerPlayer.getPlayerMark()) {
-                move.evaluation = minimax(gameGrid, humanPlayer).evaluation;
+                move.evaluation = minimax(gameGrid, humanPlayer, depth+1).evaluation;
             }
 
             else if (player.getPlayerMark() === humanPlayer.getPlayerMark()) {
-                move.evaluation = minimax(gameGrid, computerPlayer).evaluation;
+                move.evaluation = minimax(gameGrid, computerPlayer, depth+1).evaluation;
             }
 
 
             //Restoring the grid to its original state
-            gameGrid[id].innerHTML = backup;
+            gameGrid[id].moveMark = backup;
+            // gamedGrid[id].removeAttribute("moveMark");
             markedCell[id] = false;
 
             //Saving the move to further evaluate the moves based on their weight 
             moves.push(move);
+            // console.log(JSON.stringify(moves));
 
         }
 
@@ -269,7 +278,7 @@ const game = (function (gameGrid) {
 
         else {
 
-            bestEvaluation = +Infinity;
+            let bestEvaluation = +Infinity;
 
             for (let i = 0; i < moves.length; i++) {
                 if (moves[i].evaluation < bestEvaluation) {
@@ -282,6 +291,8 @@ const game = (function (gameGrid) {
 
         }
 
+
+        // console.log("best " +bestMove);
         return bestMove;
 
 
@@ -321,6 +332,8 @@ const gameBoard = (function (gameGrid) {
 
         if (!markedCell[e.target.id]) {
             e.target.innerHTML = humanPlayer.getPlayerMark();
+            e.target.moveMark = humanPlayer.getPlayerMark();
+
             markedCell[e.target.id] = true;
             turns--;
 
@@ -332,7 +345,7 @@ const gameBoard = (function (gameGrid) {
                     _declareWinner(winner.winnerMark);
                 }
 
-            }, 1);
+            }, 10);
 
 
             setTimeout(function () {
@@ -340,20 +353,20 @@ const gameBoard = (function (gameGrid) {
                     alert("game has tied");
                 }
 
-            },2)
+            },20)
 
 
             setTimeout(function () {
                 if (winner == null) {
-                    setTimeout(game.playComputerTurn, 150);
+                    setTimeout(game.playComputerTurn, 40);
 
                     setTimeout(function () {
                         winner = game.checkForWins();
                         if (winner != null)
                             _declareWinner(winner.winnerMark);
-                    }, 4);
+                    }, 50);
                 }
-            },3);
+            },30);
 
 
 
